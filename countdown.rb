43fc05ae7aa4bee2bc6af7e4d607b6ebb27717e9
Mark Crowder <crowdered@gmail.com>
2019-03-14 20:55:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,3 +5,12 @@ def countdown(num)
 end
 puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+  while num > 0 
+  puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+end
+puts "HAPPY NEW YEAR!"
+end
